8726e1469d897e55978a7411bf2a0e5c1990ae06
Tarasov Denis <dt6cgsg@gmail.com>
2021-10-26 23:10:34 +0300

    fix

--- src/test.py
+++ src/test.py
@@ -56,11 +56,11 @@ class TestStringMethods(unittest.TestCase):
 
     def test_split(self):
         """Return 1"""
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+        ss = 'hello world'
+        self.assertEqual(ss.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
-            s.split(2)
+            ss.split(2)
 
 
 if __name__ == '__main__':
