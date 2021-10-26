2e6fc2a8f9f176dd842b28f4bd6613e0f2a642d3
Tarasov Denis <dt6cgsg@gmail.com>
2021-10-26 23:08:23 +0300

    fix

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 """This module does blah blah."""
 import math
+import unittest
 
 
 print('Hello')
@@ -40,3 +41,26 @@ def kek2():
 def kek3():
     """Return 1"""
     return 12421
+
+
+class TestStringMethods(unittest.TestCase):
+    """Return 1"""
+    def test_upper(self):
+        """Return 1"""
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        """Return 1"""
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        """Return 1"""
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
