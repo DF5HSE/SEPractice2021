96a2ecde8eecc29da3afff9096e36cbfbd1d1cae
DF5HSE <fild10@yandex.ru>
2021-10-30 00:24:26 +0300

    Fix pylint and add ignoring pycache

--- src/database/user.py
+++ src/database/user.py
@@ -1,3 +1,4 @@
+"""users database"""
 import datetime
 import re
 
@@ -5,13 +6,14 @@ from datetime import date
 from typing import Dict, Tuple
 
 
-class UserMeta:
+class UserMeta:  # pylint: disable=too-few-public-methods
+    """User metadata"""
     num_of_users: int = 0
 
     def __init__(self, email: str, name: str, birth_date: date):
         self.email: str = email
         UserMeta.num_of_users += 1
-        self.id: int = UserMeta.num_of_users
+        self.identifier: int = UserMeta.num_of_users
         self.name: str = name
         self.birth_date: date = birth_date
 
@@ -21,7 +23,15 @@ id_user_meta: Dict[int, UserMeta] = {}
 email_id: Dict[str, int] = {}
 
 
-def add_user(email: str, password: str, name: str, birth_date: date) -> Tuple[bool, str]:
+def add_user(email: str, password: str,
+             name: str, birth_date: date) -> Tuple[bool, str]:
+    """
+    :param email:
+    :param password:
+    :param name:
+    :param birth_date:
+    :return:
+    """
     if re.search(r".+@.+\..+", email) is None:
         return False, "Invalid email"
     if email in email_id:
@@ -34,7 +44,7 @@ def add_user(email: str, password: str, name: str, birth_date: date) -> Tuple[bo
         return False, "Empty password"
 
     new_user_meta = UserMeta(email, name, birth_date)
-    email_id[email] = new_user_meta.id
-    id_pwd[new_user_meta.id] = password
-    id_user_meta[new_user_meta.id] = new_user_meta
+    email_id[email] = new_user_meta.identifier
+    id_pwd[new_user_meta.identifier] = password
+    id_user_meta[new_user_meta.identifier] = new_user_meta
     return True, "User added"
